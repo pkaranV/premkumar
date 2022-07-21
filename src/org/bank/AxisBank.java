da93069374d247d8636ec50ee8303ba52c99d366
pkaranV <pkaran.d0011@gmail.com>
2022-07-21 16:25:12 +0530

    new code added

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -5,6 +5,7 @@ public class AxisBank extends BankInfo{
 	public void deposit() {
 		super.deposit();
 		System.out.println("doposit is very easy");
+		System.out.println("doposit is very hard");
 
 	}
 	public static void main(String[] args) {
